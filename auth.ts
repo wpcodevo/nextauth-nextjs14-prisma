0bb6bf9bc227879b34894dca7e0a31fe069cb370
Edem <contact@codevoweb.com>
2024-02-03 08:41:18 +0000

    updated

--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import authConfig from './auth.config';
 import prisma from './prisma/prisma';
+import github from 'next-auth/providers/github';
+import google from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcryptjs';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: 'jwt' },
@@ -9,6 +12,47 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
     signIn: '/login',
   },
+  providers: [
+    github,
+    google,
+    CredentialsProvider({
+      name: 'Sign in',
+      id: 'credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'example@example.com',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: String(credentials.email),
+          },
+        });
+
+        if (
+          !user ||
+          !(await bcrypt.compare(String(credentials.password), user.password!))
+        ) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          randomKey: 'Hey cool',
+        };
+      },
+    }),
+  ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
@@ -46,5 +90,4 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return token;
     },
   },
-  ...authConfig,
 });
